356ec4cb9b96ca4a8b222de3207c3329f80eda14
Praveen kumar <109021765+16pravikumar@users.noreply.github.com>
2023-03-31 18:06:14 +0530

    Update cloneo.java

--- cloneo.java
+++ cloneo.java
@@ -1,24 +1,30 @@
-//clone object through the constructor and clone()
-public class cloneo{
-     String name;
+package basic;
+/*Use of clone()
+*/
+import java.util.*;
+import java.io.*;
+
+ public class javamain{
+	 String name;
      String email;
-  access(cloneo o){
+     javamain(javamain o){
     name=o.name;
     email=o.email;
   }
-  access(String str1,String str2)
+     javamain(String str1,String str2)
   {
     name=str1;
     email=str2;
   }
-    public static void main(String args[]){
-        cloneo obj=new cloneo("Praveen","pk113@gmail.com");
-        cloneo s=new cloneo(obj);
-        System.out.println(obj.name+" "+obj.email);
-        System.out.println(s.name+" "+s.email);
-    }
+	 public static void main(String[] args) throws CloneNotSupportedException  {
+		 javamain obj=new javamain("Praveen","pk113@gmail.com");
+		 javamain s=new javamain(obj);
+	        System.out.println(obj.name+" "+obj.email);
+	        System.out.println(s.name+" "+s.email);
+  } 
 }
-/* Output:-
--Praveen pk113@gmail.com
-Praveen pk113@gmail.com
+/*Output:
+ Praveen pk113@gmail.com
+ Praveen pk113@gmail.com
+
 */
